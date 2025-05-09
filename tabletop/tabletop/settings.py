0d24aa9200abbe14b2fcd933a0ff23416790c205
JonnyDKahle <jonnykahle@yahoo.de>
2025-05-09 16:05:21 +0200

    Add authentication views and templates for login and logout functionality

--- tabletop/tabletop/settings.py
+++ tabletop/tabletop/settings.py
@@ -131,3 +131,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = 'npc_cards:index'
+LOGOUT_REDIRECT_URL = 'npc_cards:index'
